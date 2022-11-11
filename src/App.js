7feb462f9098deefafe290fa1b31425f65f08384
islammd39e <islammdrahat308@gmail.comm>
2022-11-12 01:54:37 +0600

    upload navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { router } from "./Route/Routes/Routes";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-[1240px] mx-auto">
 
      <RouterProvider router={router}></RouterProvider>
     </div>
